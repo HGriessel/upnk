873cbe87140652e09a461f19337dfafa12588523
Hgriessel <hgriessel2@gmail.com>
2023-04-04 22:35:38 +0200

    adeded title menu

--- Readme.md
+++ Readme.md
@@ -6,15 +6,22 @@
 - sfx
 - background music
 - level or progression
+    - collect gems to buy upgrades help with progression 
+
 
 ## should have 
 
 - skill check hammer hit
+- 3 2 1 Go timer when starting game
 - add squirming pg
+- options 
+    - sound
+    - key bindings
 
 ## nice to have
 
 - animated clouds in windows
+- slow fade when starting game
 
 
 ## Baltus notes
--- interface/PuaseMenu/pause_menu.gd
+++ interface/PuaseMenu/pause_menu.gd
@@ -1,9 +1,11 @@
 extends Control
 
+@onready var start_button = $VBoxContainer/NewGame
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	if start_button != null:
+		start_button.grab_focus()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -15,3 +17,12 @@ func _on_button_pressed():
 	visible = false
 	get_tree().paused = !get_tree().paused
 	pass # Replace with function body.
+
+
+func _on_new_game_pressed():
+	get_tree().change_scene_to_file("res://Games/cup_game.tscn")
+
+
+
+func _on_quit_pressed():
+	get_tree().quit()
